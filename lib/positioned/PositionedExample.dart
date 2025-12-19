dbdc98df25a34d6061e5aed0bcd60ee26db58d1d
Samir Mangroliya <sbmangroliya2012@gmail.com>
2025-12-19 08:56:11 +0530

    Positioned widget in flutter

---
+++ lib/positioned/PositionedExample.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:fluttersecondapp/main.dart';
+
+class PositionedExample extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.orange, title: Text("ListView")),
+      body: Container(
+        color: Colors.blueGrey,
+        width: 300,
+        height: 300,
+        child: Stack(
+          children: [
+            Container(width: 100, height: 100, color: Colors.blue),
+            Positioned(
+              left: 20,
+              top: 20,
+              child: Container(width: 100, height: 100, color: Colors.green),
+            ),
+            Positioned(
+              bottom: 40,
+              right: 40,
+              child: Container(width: 100, height: 100, color: Colors.red),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
